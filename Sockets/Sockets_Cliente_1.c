1dc92b21518c9e3d5e788b2e30c15dbe18253713
Facundo Darfe <facudarfe@hotmail.com>
2022-10-04 19:06:04 -0300

    Sockets: aplicacion cliente

--- Sockets/Sockets_Cliente_1.c
+++ Sockets/Sockets_Cliente_1.c
@@ -1,5 +1,5 @@
 /***********************************************************/
-/*********************PROGRAMA SERVIDOR*********************/
+/*********************PROGRAMA CLIENTE*********************/
 /***********************************************************/
 
 // Estructura utilizada por el socket
@@ -26,14 +26,13 @@ UDP (SOCK_DGRAM) --> 	Socket de datagramas que no esta orientado a la conexion y
 #include <stdio.h>
 #include <winsock2.h>
 #include <string.h>
-#include <stdlib.h>
 
 int main(int argc, char **argv){
 	WSADATA wsa;
-	SOCKET skt, skt2;
-	int longitud_cliente, puerto = 55227, recv_size;
-	struct sockaddr_in server, cliente;
+	SOCKET skt;
+	int puerto = 55227, recv_size;
 	char mensaje[2000];
+	struct sockaddr_in server;
 	
 	/**********************INICIALIZACION WINSOCK**********************/
 	printf("Inicializando Winsock...");
@@ -53,45 +52,32 @@ int main(int argc, char **argv){
 	
 	/**********************CONFIGURACION SOCKADDR_IN**********************/	
 	server.sin_family = AF_INET; // Familia TCP/IP
-	server.sin_port = htons(puerto); // Puerto
-	server.sin_addr.s_addr = INADDR_ANY; // Cualquier cliente puede conectarse
+	server.sin_port = htons(puerto); // Puerto de la aplicacion del servidor
+	server.sin_addr.s_addr = inet_addr("127.0.0.1"); // Direccion IP del servidor
 	
-	/**********************REALIZAR EL BIND**********************/
-	if(bind(skt, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR){
-		printf("Error al realizar el bind\n");
+	/**********************CONEXION AL SERVIDOR**********************/	
+	if(connect(skt, (struct sockaddr *)&server, sizeof(server)) < 0){
+		printf("Error de conexion\n");
 		exit(-1);
 	}
-	printf("Bind realizado\n");
+	printf("Conexion exitosa\n");
 	
-	listen(skt, 5);
-	printf("Esperando conexiones entrantes...\n");
-	
-	longitud_cliente = sizeof(struct sockaddr_in);
-	skt2 = accept(skt, (struct sockaddr*)&cliente, &longitud_cliente);
-	if(skt2 == INVALID_SOCKET){
-		printf("Fallo al aceptar la conexion\n");
-		exit(-1);
-	}
-	
-	system("cls");
-	printf("Cliente %s conectado exitosamente\n", inet_ntoa(cliente.sin_addr));
-	
-	printf("Esperando mensaje entrante...\n");
-	if((recv_size = recv(skt2, mensaje, 2000, 0)) == SOCKET_ERROR)
-		printf("Recepcion fallida\n");
-	
-	mensaje[recv_size] = '\0';
-	printf("%s\n\n", mensaje);
-	
-	printf("Ingrese respuesta: "); gets(mensaje);
-	if(send(skt2, mensaje, strlen(mensaje), 0) < 0){
+	/**********************ENVIAR UN MENSAJE**********************/	
+	printf("Ingrese mensaje a enviar: "); gets(mensaje);
+	if(send(skt, mensaje, strlen(mensaje), 0) < 0){
 		printf("Error al enviar mensaje\n");
 		exit(-1);
 	}
 	
-	printf("Respuesta enviada exitosamente\n");
+	printf("Mensaje enviado exitosamente\n");
 	
-	system("pause");
+	/**********************RECEPCION DE MENSAJE**********************/	
+	printf("Esperando respuesta entrante...\n");
+	if((recv_size = recv(skt, mensaje, 2000, 0)) == SOCKET_ERROR)
+		printf("Recepcion fallida\n");
+		
+	mensaje[recv_size] = '\0';
+	printf("%s\n\n", mensaje);
 	
 	closesocket(skt);
 	WSACleanup();
